0ce18cb0debb7fc24ac2a8df1434213da3edb10f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 20:52:31 -0800

    added hex support

--- src/main/java/com/advancedkind/automessage/AutoMessage.java
+++ src/main/java/com/advancedkind/automessage/AutoMessage.java
@@ -7,7 +7,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,7 +52,7 @@ public final class AutoMessage extends JavaPlugin {
                     }
                 }
                 else {
-                    player.sendMessage("AutoMessage: Version 1.0.3");
+                    player.sendMessage("AutoMessage: Version 1.0.4");
                 }
             }
         }
--- src/main/java/com/advancedkind/automessage/tasks/Messages.java
+++ src/main/java/com/advancedkind/automessage/tasks/Messages.java
@@ -4,6 +4,9 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static net.md_5.bungee.api.ChatColor.COLOR_CHAR;
 
 public class Messages extends BukkitRunnable {
 
@@ -15,6 +18,19 @@ public class Messages extends BukkitRunnable {
         this.plugin = plugin;
     }
 
+    private static String translateHexColorCodes(final String startTag, final String endTag, final String message) {
+        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
+        final Matcher matcher = hexPattern.matcher(message);
+        final StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+        while (matcher.find()) {
+            final String group = matcher.group(1);
+            matcher.appendReplacement(buffer,
+                    COLOR_CHAR + "x" + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1) + COLOR_CHAR
+                            + group.charAt(2) + COLOR_CHAR + group.charAt(3) + COLOR_CHAR + group.charAt(4) + COLOR_CHAR
+                            + group.charAt(5));
+        }
+        return matcher.appendTail(buffer).toString();
+    }
     private int messageNum = 0;
     @Override
     public void run() {
@@ -26,7 +42,7 @@ public class Messages extends BukkitRunnable {
         String message = plugin1.getConfig().getStringList("messages").get(messageNum);
         String color = ChatColor.translateAlternateColorCodes('&', message);
         String placeholder = PlaceholderAPI.setPlaceholders(null, color);
-        plugin.getServer().broadcastMessage(placeholder);
+        plugin.getServer().broadcastMessage(translateHexColorCodes("&#", "", placeholder));
     }
     }
 }
